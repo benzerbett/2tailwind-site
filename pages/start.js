59d3b222dffff3b3abf2790ee17574873ac18d66
Benzer Bett <benzerbett@gmail.com>
2021-03-29 19:57:59 +0300

    additions

--- pages/start.js
+++ pages/start.js
@@ -3,28 +3,9 @@ import Container from '../components/layout/Container'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import SEO from '../components/SEO'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 export default function Home() {
-  const dTools = [
-    {name:'Figma', icon:'<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-figma\"><path d=\"M5 5.5A3.5 3.5 0 0 1 8.5 2H12v7H8.5A3.5 3.5 0 0 1 5 5.5z\"></path><path d=\"M12 2h3.5a3.5 3.5 0 1 1 0 7H12V2z\"></path><path d=\"M12 12.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 1 1-7 0z\"></path><path d=\"M5 19.5A3.5 3.5 0 0 1 8.5 16H12v3.5a3.5 3.5 0 1 1-7 0z\"></path><path d=\"M5 12.5A3.5 3.5 0 0 1 8.5 9H12v7H8.5A3.5 3.5 0 0 1 5 12.5z\"></path></svg>', color:'pink-600'},
-    {name:'Adobe XD', icon:'<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-figma\"><path d=\"M5 5.5A3.5 3.5 0 0 1 8.5 2H12v7H8.5A3.5 3.5 0 0 1 5 5.5z\"></path><path d=\"M12 2h3.5a3.5 3.5 0 1 1 0 7H12V2z\"></path><path d=\"M12 12.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 1 1-7 0z\"></path><path d=\"M5 19.5A3.5 3.5 0 0 1 8.5 16H12v3.5a3.5 3.5 0 1 1-7 0z\"></path><path d=\"M5 12.5A3.5 3.5 0 0 1 8.5 9H12v7H8.5A3.5 3.5 0 0 1 5 12.5z\"></path></svg>', color:'pink-900'},
-    {name:'Sketch', icon:'<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-figma\"><path d=\"M5 5.5A3.5 3.5 0 0 1 8.5 2H12v7H8.5A3.5 3.5 0 0 1 5 5.5z\"></path><path d=\"M12 2h3.5a3.5 3.5 0 1 1 0 7H12V2z\"></path><path d=\"M12 12.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 1 1-7 0z\"></path><path d=\"M5 19.5A3.5 3.5 0 0 1 8.5 16H12v3.5a3.5 3.5 0 1 1-7 0z\"></path><path d=\"M5 12.5A3.5 3.5 0 0 1 8.5 9H12v7H8.5A3.5 3.5 0 0 1 5 12.5z\"></path></svg>', color:'yellow-500'}
-  ]
-  
-  const [activeDT, setActiveDT] = useState(0)
-
-  useEffect(() => {
-    let mounted = true
-    if(mounted){
-      setTimeout(() => {
-        if(activeDT + 1 < dTools.length){setActiveDT(activeDT+1)}else{setActiveDT(0)}
-      }, 5000);
-    }
-    return () => {
-      mounted = false
-    }
-  }, [activeDT])
   return (
     <div className="flex flex-col max-w-screen max-w-900z items-center">
       <SEO title="2Tailwind" image="/logo.svg" />
@@ -46,22 +27,8 @@ export default function Home() {
         <div className="px-4 sm:px-2 sm:w-full my-1">
           <div className="grid gap-10 md:gap-16 md:grid-rows-3">
             <div 
-            // style={{
-            //   position: 'relative',
-            //   paddingBottom: '26.52%',
-            //   paddingTop: '5px',
-            //   height: '0px',
-            //   overflow: 'hidden'
-            // }}
             >
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4f3fyatDC-nlNPAZA72PuiXFxp1Sax-G9dvFPZh1AJTL8tA/viewform?embedded=true"  
-              // style={{
-              //   position: 'absolute',
-              //   top: '0',
-              //   left: '0',
-              //   width: '100%',
-              //   height: '100%'
-              // }} 
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4f3fyatDC-nlNPAZA72PuiXFxp1Sax-G9dvFPZh1AJTL8tA/viewform?embedded=true" 
               height="2413"
               width="370"
                frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
